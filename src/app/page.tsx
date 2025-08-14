5e9e3813e155f4040ecbde2d147a46f08c9cc976
Tiago Teixeira <tiagoteixeira@Tiagos-MacBook-Air.local>
2025-08-14 15:45:51 +0200

    nits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 import Image from "next/image";
 
 export default function Home() {
